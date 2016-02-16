56a82468791cea0ed8af000ab5d1ba41e87fe04a
mentrel <mentrel96@mail.ru>
2016-02-16 14:38:32 +0600

    Базовые операции и гитигнор.

--- Perwij/Perwij/Form1.Designer.cs
+++ Perwij/Perwij/Form1.Designer.cs
@@ -28,12 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(162, 91);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(44, 29);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "+";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(12, 91);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(44, 29);
+            this.button14.TabIndex = 13;
+            this.button14.Text = "X";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(62, 91);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(44, 29);
+            this.button15.TabIndex = 14;
+            this.button15.Text = "/";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(112, 91);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(44, 29);
+            this.button16.TabIndex = 15;
+            this.button16.Text = "-";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 20);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(193, 20);
+            this.textBox2.TabIndex = 17;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 65);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(193, 20);
+            this.textBox3.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button11);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+
     }
 }
 
--- Perwij/Perwij/Form1.cs
+++ Perwij/Perwij/Form1.cs
@@ -16,5 +16,75 @@ namespace Perwij
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+           string first = textBox1.Text;
+           string two = textBox2.Text;
+            int firstInt = Convert.ToInt32(first);
+            int twoInt = Convert.ToInt32(two);
+            double result = firstInt + twoInt;
+            textBox3.Text = result.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            string first = textBox1.Text;
+            string two = textBox2.Text;
+            int firstInt = Convert.ToInt32(first);
+            int twoInt = Convert.ToInt32(two);
+            double result = firstInt * twoInt;
+            textBox3.Text = result.ToString();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            string first = textBox1.Text;
+            string two = textBox2.Text;
+            int firstInt = Convert.ToInt32(first);
+            int twoInt = Convert.ToInt32(two);
+            double result = firstInt - twoInt;
+            textBox3.Text = result.ToString();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            string first = textBox1.Text;
+            string two = textBox2.Text;
+            int firstInt = Convert.ToInt32(first);
+            int twoInt = Convert.ToInt32(two);
+            double result = firstInt / twoInt;
+            textBox3.Text = result.ToString();
+        }
     }
 }
